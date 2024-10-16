e2829b9369905741029f881fbe7abf6339043016
aphilome <1.katrin.1@mail.ru>
2024-10-16 20:00:55 +0300

    first commit App_Aphilome

---
+++ frontend/lptracker-react/src/App_Aphilome.jsx
@@ -0,0 +1,9 @@
+
+function App_Aphilome() {
+    return (
+        <>
+            <p> привет </p>
+        </>
+    )
+}
+export default App_Aphilome
--- frontend/lptracker-react/src/main.jsx
+++ frontend/lptracker-react/src/main.jsx
@@ -2,12 +2,13 @@ import { StrictMode } from 'react'
 import ReactDOM from "react-dom/client";
 import App from './App.jsx'
 import App_Tbirdper from './App_Tbirdper.jsx'
+import App_Aphilome from './App_Aphilome.jsx'
 import './index.css'
 
 const rootElement =  document.getElementById('root');
 const reactRoot = ReactDOM.createRoot(rootElement);
 reactRoot.render(
   <StrictMode>
-    <App />
+    <App_Aphilome />
   </StrictMode>,
 )
