1cc1598febc1a4acf87fabc4522b27cac030dfd3
Igor Fedorov <fed-igor-1@ya.ru>
2024-10-17 21:30:10 +0300

    fix speed

--- firmware/copter/source/communicator/GlobalPositionInfo.cpp
+++ firmware/copter/source/communicator/GlobalPositionInfo.cpp
@@ -11,9 +11,9 @@ GlobalPositionInfo::GlobalPositionInfo(const mavlink_global_position_int_t& glob
     , longitude_deg(global_position.lon / 10'000'000.0f)
     , altitude_m(global_position.alt / 1000.0f)
     , relative_altitude_m(global_position.relative_alt / 1000.0f)
-    , speed_x_ms(global_position.vx)
-    , speed_y_ms(global_position.vy)
-    , speed_z_ms(global_position.vz)
+    , speed_x_ms(global_position.vx / 100.0f)
+    , speed_y_ms(global_position.vy / 100.0f)
+    , speed_z_ms(global_position.vz / 100.0f)
     , yaw_deg(global_position.hdg / 100.0f)
 {
 }
