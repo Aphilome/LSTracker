80208d79843ad466b2120f5fbb6d861143c1eec3
aphilome <1.katrin.1@mail.ru>
2024-10-19 20:47:10 +0300

    package-lock.json

--- frontend/lptracker-react/src/main.jsx
+++ frontend/lptracker-react/src/main.jsx
@@ -1,14 +1,29 @@
 import { StrictMode } from 'react'
+import { extendTheme } from '@chakra-ui/react'
 import ReactDOM from "react-dom/client";
 import App from './App.jsx'
 import App_Tbirdper from './App_Tbirdper.jsx'
 import App_Aphilome from './App_Aphilome.jsx'
 import './index.css'
+import {ChakraProvider} from "@chakra-ui/react";
+
+const colors = {
+    brand: {
+        900: '#1a365d',
+        800: '#153e75',
+        700: '#2a69ac',
+    },
+}
+
+const theme = extendTheme({ colors })
 
 const rootElement =  document.getElementById('root');
 const reactRoot = ReactDOM.createRoot(rootElement);
+
 reactRoot.render(
   <StrictMode>
-    <App_Tbirdper />
+      <ChakraProvider theme={theme}>
+          <App_Aphilome />
+      </ChakraProvider>
   </StrictMode>,
 )
