c569f91cc69cbb8200921e1ab74117126f07323b
aphilome <1.katrin.1@mail.ru>
2024-10-19 19:10:52 +0300

    add_x,y,z_coordinates_visualization

--- frontend/lptracker-react/src/App_Aphilome.jsx
+++ frontend/lptracker-react/src/App_Aphilome.jsx
@@ -1,9 +1,40 @@
 import Map from './components/Map'
 import './App.css'
+import Plot from 'react-plotly.js'
+import {DRONEPOSITIONSGPS} from "./dronePositions";
+
+function randomValues(num, mul) {
+    const arr = [];
+    const index = [];
+    for (let i = 0; i < num; i++) {
+        arr.push(Math.random() * mul)
+        index.push(i);
+    }
+    return {index, arr};
+}
 
 function App_Aphilome() {
+    const traces = Array(3).fill(0).map((_, i) => {
+        const {index, arr} = randomValues(20, 3);
+        return {
+            x: Array(20).fill(i),
+            y: index,
+            z: arr,
+            type: 'scatter3d',
+            mode: 'lines'
+        }
+    });
+
     return (
         <>
+            <Plot
+                data={traces}
+                layout={{
+                    width: 900,
+                    height: 800,
+                    title: `Simple 3D Scatter`
+                }}
+            />
             <h1 className="text-3xl font-bold underline flex-col text-red ">
                 <div className="bg-red-200">Hello world!</div>
                 <div className="text-blue-500">Hello world!</div>
